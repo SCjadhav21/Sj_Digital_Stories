634dd7cb073fc44ba98110b9effbe11d0bd32fe7
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-05-05 11:06:00 +0530

    adding authentication routes

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const { connection } = require("./Config/db");
-
+const { UserRoutes } = require("./routes/user.routes");
 app.use(express.json());
 app.use(cors());
+
+app.use("/users", UserRoutes, (req, res) => {
+  res.send(404);
+});
 
 app.listen(process.env.port, async () => {
   try {
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { Authentication } = require("../Middelware/authentication");
+const { UserModel } = require("../models/user.model");
+
+const app = express();
+app.use(express.json());
+
+const UserRoutes = express.Router();
+
+UserRoutes.post("/register", async (req, res) => {
+  let { name, email, mobile, password, address } = req.body;
+  try {
+    const users = await UserModel.find({ email });
+    if (users.length > 0) {
+      res.send({
+        msg: "email is already exist in database",
+        alert: "email is already register",
+      });
+    } else {
+      bcrypt.hash(password, 8, async (err, hash) => {
+        if (err) {
+          console.log(err);
+          res.send({ msg: err, alert: "something went wrong" });
+        } else {
+          const user = new UserModel({
+            name,
+            address,
+            email,
+            mobile,
+            password: hash,
+          });
+          await user.save();
+          res.send({
+            msg: "user Registered",
+            alert: "Registered successfully",
+          });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: err, alert: "something went wrong" });
+  }
+});
+
+UserRoutes.post("/login", async (req, res) => {
+  const { password, email } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.send({ msg: err.message, alert: "something went wrong" });
+        } else if (result) {
+          const token = jwt.sign({ userId: user._id }, process.env.key);
+
+          res.send({
+            msg: "Login Successfull",
+            alert: "Logged In",
+            token: token,
+          });
+        } else {
+          res.send({ msg: "Login Failure", alert: "Wrong Credntials" });
+        }
+      });
+    } else {
+      res.send({ msg: "Login Failure", alert: "Wrong Credntials" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: err, alert: "something went wrong" });
+  }
+});
+UserRoutes.use(Authentication);
+UserRoutes.get("/personalDetail", async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const user = await UserModel.findOne({ _id: userId });
+    res.send({ data: user, alert: "user data found" });
+  } catch (err) {
+    console.log(err);
+    res.send({ massage: err.message, alert: "Something went wrong" });
+  }
+});
+
+module.exports = { UserRoutes };
